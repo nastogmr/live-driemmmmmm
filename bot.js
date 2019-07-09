efe6fa2e8cd144c7cf46591f7884aeb51f83d6ab
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-07-09 11:56:00 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,15 +1,35 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "+";
 client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : DREAM`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
-    console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`انا زعلان من احسن لا تكلمني `,"http://twitch.tv/Dream")
-client.user.setStatus("dnd")
 });
 
-client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+
+const developers = ["595653353783033857"]
+const adminprefix = "!";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wat')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'lis')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Status You  ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/M3roof");
+      message.channel.send(`**Status You ${argresult} **`)
+}
+});
+
+
+
+
+client.login(process.env.BOT_TOKEN);
